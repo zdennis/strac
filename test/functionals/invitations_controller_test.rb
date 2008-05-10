095042ea6d29f81e3318c96c0946ef9834b7d9cb
Drew Colthorp <colthorp@atomicobject.com>
2008-05-10 18:49:16 -0500

    added a lot to the invitations controller

--- test/functionals/invitations_controller_test.rb
+++ test/functionals/invitations_controller_test.rb
@@ -14,10 +14,36 @@ class InvitationsControllerTest < Test::Unit::TestCase
   end
   
   def test_create
+    Invitation.destroy_all
+    @emails = ActionMailer::Base.deliveries
+    @emails.clear
+    
     login_as :joe
     
-    post :create, 'project_id' => projects(:project1).id, 'email_addresses' => "foo@bar.com, bar@baz.com, baz@outtaideas.com"
+    post :create, 'project_id' => projects(:project1).id, 
+      'email_addresses' => "foo@bar.com, bar@baz.com, baz@outtaideas.com",
+      'email_body' => "whattup? do my work for me"
     
     assert_redirected_to project_path(projects(:project1))
+    
+    assert_equal 3, @emails.size, "did not send an email to each to address"
+    
+    assert_equal ["foo@bar.com"], @emails[0].to, "email 0 sent to wrong address"
+    assert_match projects(:project1).name, @emails[0].subject
+    assert_match projects(:project1).name, @emails[0].body
+    assert_match "whattup? do my work for me", @emails[0].body
+    assert_match login_url(:code => Invitation.find_by_recipient("foo@bar.com").code), @emails[0].body
+    
+    assert_equal ["bar@baz.com"], @emails[1].to, "email 1 sent to wrong address"
+    assert_match projects(:project1).name, @emails[1].subject
+    assert_match projects(:project1).name, @emails[1].body
+    assert_match "whattup? do my work for me", @emails[1].body
+    assert_match login_url(:code => Invitation.find_by_recipient("bar@baz.com").code), @emails[1].body
+    
+    assert_equal ["baz@outtaideas.com"], @emails[2].to, "email 2 sent to wrong address"
+    assert_match projects(:project1).name, @emails[2].subject
+    assert_match projects(:project1).name, @emails[2].body
+    assert_match "whattup? do my work for me", @emails[2].body
+    assert_match login_url(:code => Invitation.find_by_recipient("baz@outtaideas.com").code), @emails[2].body
   end
 end
