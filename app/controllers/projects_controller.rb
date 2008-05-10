bd036695afc6d7d4d44745984717540e1ac8ce63
Zach Dennis <zach.dennis@gmail.com>
2008-05-10 19:12:49 -0400

    Implemented a points remaining trend line (thx Drew Colthorp)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,7 @@ class ProjectsController < ApplicationController
     total_points = []
     points_completed = []
     points_remaining = []
+    trends = []
     
     iterations = @project.iterations.sort_by{ |iteration| iteration.started_at }
     iterations.each do |iteration|
@@ -16,27 +17,34 @@ class ProjectsController < ApplicationController
     points_completed << @project.completed_points
     points_remaining << @project.remaining_points
     total_points << @project.total_points
-
+    
+    iteration_count = iterations.size
+    xvalues = (0..iteration_count).to_a
+    _slope = slope(xvalues, points_remaining)
+    _intercept = intercept(_slope, xvalues, points_remaining)
+    trends = xvalues.inject([]) {|values, i| values << _intercept + i*_slope }
+     
     step_count = 6
     min = 0
-    max = (points_completed + total_points + points_remaining).max
+    max = (points_completed + total_points + points_remaining).map(&:to_i).max
     step = max / step_count.to_f
     ylabels = []
     min.step(max, step) { |f| ylabels << f.round }
     xlabels = iterations.map{ |e| iterations.index(e) } + ["current"]
 
     red = 'FF0000'
-    yellow = 'FFFF00'
+    dark_purple = '551a8b'
     green = '00FF00'
     blue = '0000FF'
+    purple = 'a020f0'
     
     chart = Gchart.new(
-     :data =>       [total_points, points_remaining, points_completed], 
-     :bar_colors => [blue,         green,            yellow],
+     :data =>       [total_points, points_completed, points_remaining, trends], 
+     :bar_colors => [blue,         green,            purple, dark_purple],
      :size => "600x200",
      :axis_with_labels => ["x", "y"],
      :axis_labels => [xlabels, ylabels],
-     :legend => ["Total Points", "Points Remaining", "Total Points Completed"]
+     :legend => ["Total Points", "Points Remaining", "Total Points Completed", "Points Remaining Trend"]
     )
     
     render :text => chart.send!(:fetch)
@@ -106,4 +114,12 @@ class ProjectsController < ApplicationController
       redirect_to "/access_denied.html"
     end
   end
+  
+  private
+  
+  def sum(arr) arr.inject(0.0){|sum, x| sum + x} end
+  def slope(xs, ys); n = xs.size; (n*sum(xs.zip(ys).map{|x,y| x*y}) - sum(xs)*sum(ys))/(n*sum(xs.map{|x| x*x}) - sum(xs)**2) end
+  def intercept(m, xs,ys) (sum(ys) - m*sum(xs))/xs.size; end
+    
+
 end
