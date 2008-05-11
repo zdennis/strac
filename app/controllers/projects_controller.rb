f9536b83fca0fc1eb6bed8ebb4cb6e0cae39e333
Drew Colthorp <colthorp@atomicobject.com>
2008-05-11 14:47:14 -0500

    extracted sum_squares, sum_products from slope

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -111,9 +111,30 @@ class ProjectsController < ApplicationController
   
   private
   
-  def sum(arr) arr.inject(0.0){|sum, x| sum + x} end
-  def slope(xs, ys); n = xs.size; (n*sum(xs.zip(ys).map{|x,y| x*y}) - sum(xs)*sum(ys))/(n*sum(xs.map{|x| x*x}) - sum(xs)**2) end
-  def intercept(m, xs,ys) (sum(ys) - m*sum(xs))/xs.size; end
-    
-
+  def sum(arr)
+    arr.inject(0.0){ |sum, x|
+      sum + x
+    }
+  end
+  
+  def sum_products(xs, ys)
+    xs.zip(ys).inject{|result, (x,y)|
+      result + x*y
+    }
+  end
+  
+  def sum_squares(xs)
+    xs.inject(0.0) {|result, x|
+      result + x*x
+    }
+  end
+  
+  def slope(xs, ys)
+    n = xs.size
+    (n*sum_products(xs, ys) - sum(xs)*sum(ys)) / (n*sum_squares(xs) - sum(xs)**2)
+  end
+  
+  def intercept(m, xs,ys)
+    (sum(ys) - m*sum(xs))/xs.size
+  end
 end
