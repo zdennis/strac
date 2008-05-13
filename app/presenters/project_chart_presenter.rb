6b6dc286e4fbe12eb9be27265bff8a734ad0476b
Drew Colthorp <colthorp@atomicobject.com>
2008-05-12 21:22:43 -0500

    pulled total_points out of ProjectsController. Pulled iterations out of points_completed

--- app/presenters/project_chart_presenter.rb
+++ app/presenters/project_chart_presenter.rb
@@ -3,10 +3,19 @@ class ProjectChartPresenter < PresentationObject
     @project = project
   end
   
+  declare :iterations do
+    @project.iterations.sort_by{ |iteration| iteration.started_at }
+  end
+  
   declare :points_completed do
-    iterations = @project.iterations.sort_by{ |iteration| iteration.started_at }
     points_completed = iterations.map{|iteration| iteration.snapshot.completed_points}
     points_completed << @project.completed_points
     points_completed
+  end
+  
+  declare :total_points do
+    total_points = iterations.map{|iteration| iteration.snapshot.total_points}
+    total_points << @project.total_points
+    total_points
   end
 end
