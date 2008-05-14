9d4823128c7e816f345e12d20b5b3163994b9638
Zach Dennis <zach.dennis@gmail.com>
2008-05-13 23:13:17 -0400

    added specs against StoriesController#new/edit actions to test finding/setting statuses and priorities

--- spec/controllers/stories_controller/stories_controller_edit_spec.rb
+++ spec/controllers/stories_controller/stories_controller_edit_spec.rb
@@ -12,12 +12,28 @@ describe StoriesController, '#edit' do
     @stories = stub("stories", :find => nil)
     ProjectPermission.stub!(:find_project_for_user).and_return(@project)
     @project.stub!(:stories).and_return(@stories)
+    Status.stub!(:find).with(:all).and_return([])
+    Priority.stub!(:find).with(:all).and_return([])
   end
 
   it "finds and assigns the @project for the requested story" do
     ProjectPermission.should_receive(:find_project_for_user).with(@project.id.to_s, @user).and_return(@project)
     get_edit
     assigns[:project].should == @project
+  end
+
+  it "finds and assigns statuses" do
+    statuses = [ mock_model(Status, :name => "Foo"), mock_model(Status, :name => "Baz") ]
+    Status.should_receive(:find).with(:all).and_return(statuses)
+    get_edit
+    assigns[:statuses].should == [ [], ["Foo", statuses.first.id], ["Baz", statuses.last.id] ]
+  end
+  
+  it "finds and assigns priorities" do
+    priorities = [ mock_model(Priority, :name => "Baz"), mock_model(Priority, :name => "Bar") ]
+    Priority.should_receive(:find).with(:all).and_return(priorities)
+    get_edit
+    assigns[:priorities].should == [ [], ["Baz", priorities.first.id], ["Bar", priorities.last.id] ]
   end
   
   describe "when a project can't be found" do
