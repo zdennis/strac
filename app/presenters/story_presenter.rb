e9e8c44246eba962e8dd09effdd4e2f6669fd657
Zach Dennis <zach.dennis@gmail.com>
2008-05-13 23:34:23 -0400

    Implemented the initial requirements for the StoryPresenter

--- app/presenters/story_presenter.rb
+++ app/presenters/story_presenter.rb
@@ -1,2 +1,16 @@
 class StoryPresenter < PresentationObject
+  def initialize(options)
+    @story = options[:story]
+  end
+  
+  delegate :class, :id, :new_record?, :to_param,
+           :bucket_id, :description, :points, :priority_id, 
+           :responsible_party_type_id, :status_id, :summary, :tag_list,
+           :to => :@story
+
+  
+  declare :possible_statuses do
+    Status.find(:all).map{ |status| [status.name, status.id] }.unshift []
+  end
+  
 end
