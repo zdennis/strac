bf245a2d0e7da387c58b98df23a8ed009a37d509
Drew Colthorp <colthorp@atomicobject.com>
2008-05-25 16:58:18 -0500

    changes to render_with to make it easier to test

--- app/renderers/stories_renderer.rb
+++ app/renderers/stories_renderer.rb
@@ -1,6 +1,6 @@
 class StoriesRenderer < Renderer
   def add_to_story_list story
     bucket_id = "iteration_#{story.bucket ? story.bucket.id : 'nil'}"
-    @page.insert_html :bottom, bucket_id, :partial => 'list', :locals => { :stories => [ story ] }
+    page.insert_html :bottom, bucket_id, :partial => 'list', :locals => { :stories => [ story ] }
   end
 end
