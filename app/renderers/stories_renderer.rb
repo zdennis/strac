f9fe20cf8b3162a41e6da083a59b2a4e457a138d
Drew Colthorp <colthorp@atomicobject.com>
2008-05-25 17:06:46 -0500

    pulled out StoriesRenderer#make_new_story_sortable_in_project

--- app/renderers/stories_renderer.rb
+++ app/renderers/stories_renderer.rb
@@ -1,6 +1,17 @@
 class StoriesRenderer < Renderer
   def add_to_story_list story
-    bucket_id = "iteration_#{story.bucket ? story.bucket.id : 'nil'}"
-    page.insert_html :bottom, bucket_id, :partial => 'list', :locals => { :stories => [ story ] }
+    page.insert_html :bottom, bucket_id(story), :partial => 'list', :locals => { :stories => [ story ] }
+  end
+  
+  def make_new_story_sortable_in_project story, project
+    iteration_list_ids = [ "iteration_nil" ] + project.iterations.map{ |i| "iteration_#{i.id}" }
+    page.sortable bucket_id(story), :url => reorder_stories_path(project), :method => :put,
+                                :tag => 'div', :handle => 'draggable', :dropOnEmpty => true, :containment => iteration_list_ids
+  end
+  
+  private
+  
+  def bucket_id(story)
+    "iteration_#{story.bucket ? story.bucket.id : 'nil'}"
   end
 end
