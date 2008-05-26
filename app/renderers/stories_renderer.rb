ba72e1b4b82014b872ea59cac393c6f5b5605020
Drew Colthorp <colthorp@atomicobject.com>
2008-05-26 13:05:57 -0500

    renamed bucket_id helper in storiesRenderer to bucket_element_id. Had it take a bucket_id instead of a story. Changed clear_new_story_form to take a bucket_id instead of a story, since that makes a lot more sense

--- app/renderers/stories_renderer.rb
+++ app/renderers/stories_renderer.rb
@@ -1,22 +1,23 @@
 class StoriesRenderer < Renderer
   def add_to_story_list story
-    page.insert_html :bottom, bucket_id(story), :partial => 'list', :locals => { :stories => [ story ] }
+    page.insert_html :bottom, bucket_element_id(story.bucket_id), :partial => 'list', :locals => { :stories => [ story ] }
   end
   
   def make_new_story_sortable_in_project story, project
     iteration_list_ids = [ "iteration_nil" ] + project.iterations.map{ |i| "iteration_#{i.id}" }
-    page.sortable bucket_id(story), :url => reorder_stories_path(project), :method => :put,
+    page.sortable bucket_element_id(story.bucket_id), :url => reorder_stories_path(project), :method => :put,
                                 :tag => 'div', :handle => 'draggable', :dropOnEmpty => true, :containment => iteration_list_ids
   end
   
-  def clear_new_story_form story
-    blank_story = Story.new :bucket_id => story.bucket_id # we want a fresh form
-    page["#{bucket_id(story)}_story_new"].replace_html :partial => 'stories/new', :locals => { :story => blank_story }    
+  def clear_new_story_form bucket_id
+    blank_story = Story.new :bucket_id => bucket_id # we want a fresh form
+    story_form_id = "#{bucket_element_id(bucket_id)}_story_new"
+    page[story_form_id].replace_html :partial => 'stories/new', :locals => { :story => blank_story }    
   end
   
   private
   
-  def bucket_id(story)
-    "iteration_#{story.bucket ? story.bucket.id : 'nil'}"
+  def bucket_element_id(bucket_id)
+    "iteration_#{bucket_id ? bucket_id : 'nil'}"
   end
 end
