df3505f48d43bbe148c3a25519907cb4d14b808d
Zach Dennis <zach.dennis@gmail.com>
2008-05-26 16:07:16 -0400

    Inlined Project#incomplete_stories into ProjectsController#workspace for refactoring example

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -335,16 +335,34 @@ describe ProjectsController, '#workspace' do
   
   describe 'when the user has access to the project' do
     before do
-      @project = mock_model(Project, :incomplete_stories => [])
+      @project = Generate.project
+      @incomplete_stories = []
+      story_count = 3
+      story_count.times do |i|
+        @incomplete_stories << Generate.story(:project => @project) 
+        @incomplete_stories.last.update_attribute(:position, story_count - i)
+      end
+      @sorted_incomplete_stories = @incomplete_stories.sort_by{ |story| story.position }    
       ProjectPermission.stub!(:find_project_for_user).and_return(@project)
     end
     
-    it "assigns @stories" do
-      @stories = mock(:stories)
-      @project.should_receive(:incomplete_stories).with().and_return(@stories)
+    it "assigns @stories to the project's incomplete stories" do
       get_workspace
-      assigns(:stories).should == @stories
+      assigns(:stories).should == @sorted_incomplete_stories
     end
+    
+    describe "when there are completed stories" do
+      before do
+        @completed_stories = []
+        3.times { @completed_stories << Generate.story(:project => @project, :status => Status.complete) }
+      end
+    
+      it "does not include completed stories" do
+        get_workspace
+        assigns(:stories).should == @sorted_incomplete_stories
+      end
+    end
+    
   end
   
   describe 'when the user does not have access to the project' do
