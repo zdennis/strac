ea184ddc84a59054f7ab8c1b820e4bde5abdc6a9
Drew Colthorp <colthorp@atomicobject.com>
2008-05-26 22:24:01 -0500

    moved chart below other actions in ProjectsController

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,67 @@
 class ProjectsController < ApplicationController
   restrict_to :user
   
+  def index
+    @projects = ProjectPermission.find_all_projects_for_user(current_user)
+  end
+
+  def show
+    @project = ProjectPermission.find_project_for_user(params[:id], current_user)
+    redirect_to "/access_denied.html" unless @project
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+    @project = ProjectPermission.find_project_for_user(params[:id], current_user)
+    redirect_to "/access_denied.html" unless @project
+  end
+
+  def create
+    @project = Project.new(params[:project])
+    
+    if @project.save 
+      current_user.projects << @project
+      flash[:notice] = 'Project was successfully created.'
+      redirect_to project_path(@project)
+    else
+      render :action => "new"
+    end
+  end
+
+  def update
+    @project = ProjectPermission.find_project_for_user(params[:id], current_user)
+    if @project && @project.update_attributes(params[:project])
+      @project.update_members params[:users]
+      flash[:notice] = 'Project was successfully updated.'
+      redirect_to project_path(@project)
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    if @project=ProjectPermission.find_project_for_user(params[:id], current_user)
+      @project.destroy
+      redirect_to projects_path
+    else
+      redirect_to "/access_denied.html"
+    end
+  end
+  
+  def workspace
+    if @project=ProjectPermission.find_project_for_user(params[:id], current_user)
+      @stories = @project.stories.find(
+        :all, 
+        :conditions => ["status_id NOT IN(?) OR status_id IS NULL", Status.complete], :order => "position ASC"
+      )
+    else
+      redirect_to "/access_denied.html"
+    end
+  end
+
   def chart
     @project=ProjectPermission.find_project_for_user(params[:id], current_user)
     total_points = []
@@ -62,67 +123,6 @@ class ProjectsController < ApplicationController
     )
     
     render :text => chart.send!(:fetch)
-  end
-
-  def index
-    @projects = ProjectPermission.find_all_projects_for_user(current_user)
-  end
-
-  def show
-    @project = ProjectPermission.find_project_for_user(params[:id], current_user)
-    redirect_to "/access_denied.html" unless @project
-  end
-
-  def new
-    @project = Project.new
-  end
-
-  def edit
-    @project = ProjectPermission.find_project_for_user(params[:id], current_user)
-    redirect_to "/access_denied.html" unless @project
-  end
-
-  def create
-    @project = Project.new(params[:project])
-    
-    if @project.save 
-      current_user.projects << @project
-      flash[:notice] = 'Project was successfully created.'
-      redirect_to project_path(@project)
-    else
-      render :action => "new"
-    end
-  end
-
-  def update
-    @project = ProjectPermission.find_project_for_user(params[:id], current_user)
-    if @project && @project.update_attributes(params[:project])
-      @project.update_members params[:users]
-      flash[:notice] = 'Project was successfully updated.'
-      redirect_to project_path(@project)
-    else
-      render :action => "edit"
-    end
-  end
-
-  def destroy
-    if @project=ProjectPermission.find_project_for_user(params[:id], current_user)
-      @project.destroy
-      redirect_to projects_path
-    else
-      redirect_to "/access_denied.html"
-    end
-  end
-  
-  def workspace
-    if @project=ProjectPermission.find_project_for_user(params[:id], current_user)
-      @stories = @project.stories.find(
-        :all, 
-        :conditions => ["status_id NOT IN(?) OR status_id IS NULL", Status.complete], :order => "position ASC"
-      )
-    else
-      redirect_to "/access_denied.html"
-    end
   end
   
   private
