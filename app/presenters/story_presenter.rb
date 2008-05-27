17e8775d206fcd624a5c027ad70aa75a01408cfc
Zach Dennis <zach.dennis@gmail.com>
2008-05-27 15:16:30 -0400

    added #possible_buckets method to StoryPresenter, updated the stories/form partial to use it

--- app/presenters/story_presenter.rb
+++ app/presenters/story_presenter.rb
@@ -11,6 +11,16 @@ class StoryPresenter
            :summary, :tag_list, 
            :to => :@story
   
+  def possible_buckets
+    bucket_hash = project.buckets.group_by(&:type)
+    buckets = bucket_hash.keys.inject([]) do |arr,key| 
+      values = bucket_hash[key]
+      def values.name ; first.type; end
+      def values.group ; self ; end
+      arr << values
+    end
+  end
+
   def possible_priorities
     Priority.find(:all).map{ |e| [e.name, e.id] }.unshift []
   end
